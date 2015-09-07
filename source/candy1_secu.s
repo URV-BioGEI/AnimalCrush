48ac4414a0e5ad9634cc1c1d98ce5ec1ed4428df
Santiago Romani <santiago.romani@urv.cat>
2015-09-07 16:27:23 +0200

    prog2: incluye candy1_secu.s

---
+++ source/candy1_secu.s
@@ -0,0 +1,120 @@
+@;=                                                               		=
+@;=== candy1_secu.s: rutinas para detectar y elimnar secuencias 	  ===
+@;=                                                             	  	=
+@;=== Programador tarea 1C: xxx.xxx@estudiants.urv.cat				  ===
+@;=== Programador tarea 1D: yyy.yyy@estudiants.urv.cat				  ===
+@;=                                                           		   	=
+
+
+
+.include "../include/candy1_incl.i"
+
+
+
+@;-- .bss. variables (globales) no inicializadas ---
+.bss
+		.align 2
+@; número de secuencia: se utiliza para generar números de secuencia únicos,
+@;	(ver rutinas 'marcar_horizontales' y 'marcar_verticales') 
+	num_sec:	.space 1
+
+
+
+@;-- .text. código de las rutinas ---
+.text	
+		.align 2
+		.arm
+
+
+
+@;TAREA 1C;
+@; hay_secuencia(*matriz): rutina para detectar si existe, por lo menos, una
+@;	secuencia de tres elementos iguales consecutivos, en horizontal o en
+@;	vertical, incluyendo elementos en gelatinas simples y dobles.
+@;	Restricciones:
+@;		* para detectar secuencias se invocará la rutina 'cuenta_repeticiones'
+@;			(ver fichero "candy1_move.s")
+@;	Parámetros:
+@;		R0 = dirección base de la matriz de juego
+@;	Resultado:
+@;		R0 = 1 si hay una secuencia, 0 en otro caso
+	.global hay_secuencia
+hay_secuencia:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+
+@;TAREA 1D;
+@; elimina_secuencias(*matriz, *marcas): rutina para eliminar todas las
+@;	secuencias de 3 o más elementos repetidos consecutivamente en horizontal,
+@;	vertical o combinaciones, así como de reducir el nivel de gelatina en caso
+@;	de que alguna casilla se encuentre en dicho modo; 
+@;	además, la rutina marca todos los conjuntos de secuencias sobre una matriz
+@;	de marcas que se pasa por referencia, utilizando un identificador único para
+@;	cada conjunto de secuencias (el resto de las posiciones se inicializan a 0). 
+@;	Parámetros:
+@;		R0 = dirección base de la matriz de juego
+@;		R1 = dirección de la matriz de marcas
+	.global elimina_secuencias
+elimina_secuencias:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+	
+@;:::RUTINAS DE SOPORTE:::
+
+
+
+@; marcar_horizontales(mat): rutina para marcar todas las secuencias de 3 o más
+@;	elementos repetidos consecutivamente en horizontal, con un número identifi-
+@;	cativo diferente para cada secuencia, que empezará siempre por 1 y se irá
+@;	incrementando para cada nueva secuencia, y cuyo último valor se guardará en
+@;	la variable global 'num_sec'; las marcas se guardarán en la matriz que se
+@;	pasa por parámetro 'mat' (por referencia).
+@;	Restricciones:
+@;		* se supone que la matriz 'mat' está toda a ceros
+@;		* para detectar secuencias se invocará la rutina 'cuenta_repeticiones'
+@;			(ver fichero "candy1_move.s")
+@;	Parámetros:
+@;		R0 = dirección base de la matriz de juego
+@;		R1 = dirección de la matriz de marcas
+marcar_horizontales:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+
+@; marcar_verticales(mat): rutina para marcar todas las secuencias de 3 o más
+@;	elementos repetidos consecutivamente en vertical, con un número identifi-
+@;	cativo diferente para cada secuencia, que seguirá al último valor almacenado
+@;	en la variable global 'num_sec'; las marcas se guardarán en la matriz que se
+@;	pasa por parámetro 'mat' (por referencia);
+@;	sin embargo, habrá que preservar los identificadores de las secuencias
+@;	horizontales que intersecten con las secuencias verticales, que se habrán
+@;	almacenado en en la matriz de referencia con la rutina anterior.
+@;	Restricciones:
+@;		* se supone que la matriz 'mat' está marcada con los identificadores
+@;			de las secuencias horizontales
+@;		* la variable 'num_sec' contendrá el siguiente indentificador (>1)
+@;		* para detectar secuencias se invocará la rutina 'cuenta_repeticiones'
+@;			(ver fichero "candy1_move.s")
+@;	Parámetros:
+@;		R0 = dirección base de la matriz de juego
+@;		R1 = dirección de la matriz de marcas
+marcar_verticales:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+
+.end
