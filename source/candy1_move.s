8b99732d826a388b8059261a2ed9e03d78c2534f
Aleix Marine <aleix.marine@estudiants.urv.cat>
2016-09-21 17:50:56 +0200

    prog3:Funció Detecta_secuencia

--- source/candy1_move.s
+++ source/candy1_move.s
@@ -1,8 +1,8 @@
 @;=                                                         	      	=
 @;=== candy1_move: rutinas para contar repeticiones y bajar elementos ===
 @;=                                                          			=
-@;=== Programador tarea 1E: xxx.xxx@estudiants.urv.cat				  ===
-@;=== Programador tarea 1F: yyy.yyy@estudiants.urv.cat				  ===
+@;=== Programador tarea 1E: Aleix.marine@estudiants.urv.cat			  ===
+@;=== Programador tarea 1F: Aleix.marine@estudiants.urv.cat			  ===
 @;=                                                         	      	=
 
 
@@ -37,12 +37,82 @@
 @;		R3 = orientación 'ori' (0 -> Este, 1 -> Sur, 2 -> Oeste, 3 -> Norte)
 @;	Resultado:
 @;		R0 = número de repeticiones detectadas (mínimo 1)
+@;  Variables associades a registres:
+@;  	r4 = repeticions de l'element
+@;		r5 = tres bits de menys pes del primer element
+@;		r6 = Apuntador-> matriu + (i*COLUMNS+j)
+@;		r7 = tres bits de menys pes de l'element actual
+
 	.global cuenta_repeticiones
 cuenta_repeticiones:
-		push {lr}
-		
-		
-		pop {pc}
+		push {r4-r10,lr}
+		@;Seccio ENTRADA
+		mov r4, #1 					@;r4: repeticions de l'element=1
+		mov r10, #COLUMNS 			@;r10 registre temporal per a guardar el valor de la constant COLUMNS
+		mla r7, r1, r10, r2 		@;Obtenim a r7 i*COLUMNS + j
+		add r6, r7, r0 				@;Obtenim a r6 i*COLUMNS+j+@matriu, obtenint la direcció a la que apunta el primer element
+		ldrb r8, [r6]				@;Carreguem a registres el contingut de la posició actual de la matriu
+		and r5, r8, #0x00000007		@;Fem una màscara, posant tots els bits a 0 excepte els 3 ultims que mantindran el seu valor a r5
+		cmp r3, #1					@;comparem ori amb 1
+		bgt .Mesgran				@;si es mes gran es 2 o 3 ves a mes gran
+		beq .Sud					@;Si es 1 vol dir que ori es sud  i ves a sud
+		@;Seccio EST
+		.Est:
+		cmp r2, #COLUMNS			@;Comparem amb COLUMNS
+		bge .Exit					@;Si es mes gran o igual ves a la seccio de sortida
+		add r6, #1 					@;Passem al següent element
+		add r2, #1					@;Modifiquem l'index sumant 1
+		ldrb r8, [r6]				@;Carreguem a r8 el contingut de memoria al que apunta r6 (següent element)
+		and r7, r8, #0x00000007		@;Tres bits de menys pes de l'element actual
+		cmp r5, r7					@;Comparem els tres bits de menys pes de l'element actual amb el primer element
+		bne .Exit					@;Si son diferents ves a la seccio exit
+		add r4, #1					@;Afegeix repeticio perque son iguals
+		b .Est
+		.Sud:
+		@;Seccio SUD
+		cmp r2, #ROWS				@;Comparem amb ROWS
+		bge .Exit					@;Si es mes gran o igual ves a la seccio de sortida
+		mov r8, #COLUMNS			@;Carreguem el valor de COLUMNS a r8
+		add r6, r8		 			@;Passem al següent element
+		add r1, #1					@;Modifiquem l'index sumant 1
+		ldrb r8, [r6]				@;Carreguem a r8 el contingut de memoria al que apunta r6 (següent element)
+		and r7, r8, #0x00000007		@;Tres bits de menys pes de l'element actual a r7
+		cmp r5, r7					@;Comparem els tres bits de menys pes de l'element actual amb el primer element
+		bne .Exit					@;Si son diferents ves a la seccio exit
+		add r4, #1					@;Afegeix repeticio perque son iguals
+		b .Sud						@;Torna a començar el bucle de recorregut
+		.Mesgran:
+		cmp r3, #2
+		beq .Oest
+		@;Seccio NORD
+		.Nord:
+		cmp r1, #0					@;Comparem amb 0
+		ble .Exit					@;Si es mes petit o igual ves a la seccio de sortida
+		mov r8, #COLUMNS			@;Carreguem el valor de COLUMNS a r8
+		sub r6, r8					@;Passem al següent element
+		sub r1, #1					@;Modifiquem l'index restant 1
+		ldrb r8, [r6]				@;Carreguem a r8 el contingut de memoria al que apunta r6 (següent element)
+		and r7, r8, #0x00000007		@;Tres bits de menys pes de l'element actual a r7
+		cmp r5, r7					@;Comparem els tres bits de menys pes de l'element actual amb el primer element
+		bne .Exit					@;Si son diferents ves a la seccio exit
+		add r4, #1					@;Afegeix repeticio perque son iguals
+		b .Nord						@;Torna a començar el bucle de recorregut
+		.Oest:
+		@;Seccio OEST
+		cmp r2, #0
+		ble .Exit
+		sub r6, #1 					@;Passem al següent element
+		sub r2, #1					@;Modifiquem l'index restant 1
+		ldrb r8, [r6]				@;Carreguem a r8 el contingut de memoria al que apunta r6 (següent element)
+		and r7, r8, #0x00000007		@;Tres bits de menys pes de l'element actual
+		cmp r5, r7					@;Comparem els tres bits de menys pes de l'element actual amb el primer element
+		bne .Exit					@;Si son diferents ves a la seccio exit
+		add r4, #1					@;Afegeix repeticio perque son iguals
+		b .Oest
+		@;Seccio EXIT El programa sempre acabara aqui, per tant fem les operacions pertinents de sortida
+		.Exit:
+		mov r0, r4
+		pop {r4-r10, pc}
 
 
 
@@ -80,9 +150,18 @@ baja_elementos:
 @;		R4 = dirección base de la matriz de juego
 @;	Resultado:
 @;		R0 = 1 indica que se ha realizado algún movimiento. 
+@;	Variables associades a registres
+@;		r1 = fila
+@;		r2 = columna
+@;		r3 = apuntador de posicio actual
+@;		r4 = direccio base
+@;		r5 = bits de menys pes de l'element actual
+@;		r6 = apuntador de posicio inicial en un tractament
+@;		r7 = apuntador de fila en un tractament
+@;		r8 = contingut de la posicio a tractar (sera 0, 8 o 16)
+@; 		r10 = emmagatzema si hi ha hagut moviments (r0 esta en ús tota l'estona per mod random)
 baja_verticales:
 		push {lr}
-		
 		
 		pop {pc}
 
