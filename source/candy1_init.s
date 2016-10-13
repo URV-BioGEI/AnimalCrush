5c01f01586e97f922fcccd2c2821c7347b90b882
Aleix Marine <aleix.marine@estudiants.urv.cat>
2016-10-13 10:50:11 +0200

    master: fitxers de practica (buits)

---
+++ source/candy1_init.s
@@ -0,0 +1,126 @@
+@;=                                                          	     	=
+@;=== candy1_init.s: rutinas para inicializar la matriz de juego	  ===
+@;=                                                           	    	=
+@;=== Programador tarea 1A: xxx.xxx@estudiants.urv.cat				  ===
+@;=== Programador tarea 1B: yyy.yyy@estudiants.urv.cat				  ===
+@;=                                                       	        	=
+
+
+
+.include "../include/candy1_incl.i"
+
+
+
+@;-- .bss. variables (globales) no inicializadas ---
+.bss
+		.align 2
+@; matrices de recombinación: matrices de soporte para generar una nueva matriz
+@;	de juego recombinando los elementos de la matriz original.
+	mat_recomb1:	.space ROWS*COLUMNS
+	mat_recomb2:	.space ROWS*COLUMNS
+
+
+
+@;-- .text. código de las rutinas ---
+.text	
+		.align 2
+		.arm
+
+
+
+@;TAREA 1A;
+@; inicializa_matriz(*matriz, num_mapa): rutina para inicializar la matriz de
+@;	juego, primero cargando el mapa de configuración indicado por parámetro (a
+@;	obtener de la variable global 'mapas'), y después cargando las posiciones
+@;	libres (valor 0) o las posiciones de gelatina (valores 8 o 16) con valores
+@;	aleatorios entre 1 y 6 (+8 o +16, para gelatinas)
+@;	Restricciones:
+@;		* para obtener elementos de forma aleatoria se invocará la rutina
+@;			'mod_random'
+@;		* para evitar generar secuencias se invocará la rutina
+@;			'cuenta_repeticiones' (ver fichero "candy1_move.s")
+@;	Parámetros:
+@;		R0 = dirección base de la matriz de juego
+@;		R1 = número de mapa de configuración
+	.global inicializa_matriz
+inicializa_matriz:
+		push {lr}		@;guardar registros utilizados
+		
+		
+		pop {pc}			@;recuperar registros y volver
+
+
+
+@;TAREA 1B;
+@; recombina_elementos(*matriz): rutina para generar una nueva matriz de juego
+@;	mediante la reubicación de los elementos de la matriz original, para crear
+@;	nuevas jugadas.
+@;	Inicialmente se copiará la matriz original en 'mat_recomb1', para luego ir
+@;	escogiendo elementos de forma aleatoria y colocandolos en 'mat_recomb2',
+@;	conservando las marcas de gelatina.
+@;	Restricciones:
+@;		* para obtener elementos de forma aleatoria se invocará la rutina
+@;			'mod_random'
+@;		* para evitar generar secuencias se invocará la rutina
+@;			'cuenta_repeticiones' (ver fichero "candy1_move.s")
+@;		* para determinar si existen combinaciones en la nueva matriz, se
+@;			invocará la rutina 'hay_combinacion' (ver fichero "candy1_comb.s")
+@;		* se supondrá que siempre existirá una recombinación sin secuencias y
+@;			con combinaciones
+@;	Parámetros:
+@;		R0 = dirección base de la matriz de juego
+	.global recombina_elementos
+recombina_elementos:
+		push {lr}
+		
+		
+		pop {pc}
+
+
+
+@;:::RUTINAS DE SOPORTE:::
+
+
+
+@; mod_random(n): rutina para obtener un número aleatorio entre 0 y n-1,
+@;	utilizando la rutina 'random'
+@;	Restricciones:
+@;		* el parámetro 'n' tiene que ser un valor entre 2 y 255, de otro modo,
+@;		  la rutina lo ajustará automáticamente a estos valores mínimo y máximo
+@;	Parámetros:
+@;		R0 = el rango del número aleatorio (n)
+@;	Resultado:
+@;		R0 = el número aleatorio dentro del rango especificado (0..n-1)
+	.global mod_random
+mod_random:
+		push {r1-r4, lr}
+		
+		
+		pop {r1-r4, pc}
+
+
+
+@; random(): rutina para obtener un número aleatorio de 32 bits, a partir de
+@;	otro valor aleatorio almacenado en la variable global 'seed32' (declarada
+@;	externamente)
+@;	Restricciones:
+@;		* el valor anterior de 'seed32' no puede ser 0
+@;	Resultado:
+@;		R0 = el nuevo valor aleatorio (también se almacena en 'seed32')
+random:
+	push {r1-r5, lr}
+		
+	ldr r0, =seed32				@;R0 = dirección de la variable 'seed32'
+	ldr r1, [r0]				@;R1 = valor actual de 'seed32'
+	ldr r2, =0x0019660D
+	ldr r3, =0x3C6EF35F
+	umull r4, r5, r1, r2
+	add r4, r3					@;R5:R4 = nuevo valor aleatorio (64 bits)
+	str r4, [r0]				@;guarda los 32 bits bajos en 'seed32'
+	mov r0, r5					@;devuelve los 32 bits altos como resultado
+		
+	pop {r1-r5, pc}	
+
+
+
+.end
