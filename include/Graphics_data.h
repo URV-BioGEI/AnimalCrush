b73e413e1e1ed4646eaf021d791b5d3c5972cb40
Albert <albert.canellas@estudiants.urv.cat>
2016-12-19 22:44:00 +0100

    Prog4:grafics canviats pels animalicos

--- include/Graphics_data.h
+++ include/Graphics_data.h
@@ -6,16 +6,17 @@
 
 ------------------------------------------------------------------------------*/
 
+
 //{{BLOCK(Fondo)
 
 //======================================================================
 //
-//	Fondo, 256x512@16, 
+//	Fondo, 512x256@16, 
 //	Alphabit on.
 //	+ bitmap lz77 compressed
-//	Total size: 180096 = 180096
+//	Total size: 228368 = 228368
 //
-//	Time-stamp: 2014-09-24, 09:24:46
+//	Time-stamp: 2016-12-18, 16:00:48
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -24,8 +25,8 @@
 #ifndef GRIT_FONDO_H
 #define GRIT_FONDO_H
 
-#define FondoBitmapLen 189416
-extern const unsigned short FondoBitmap[94708];
+#define FondoBitmapLen 228368
+extern const unsigned short FondoBitmap[114184];
 
 #endif // GRIT_FONDO_H
 
@@ -63,18 +64,16 @@ extern const unsigned int BaldosasPal[41];
 //}}BLOCK(Baldosas)
 
 
-
 //{{BLOCK(Sprites)
 
 //======================================================================
 //
 //	Sprites, 192x32@8, 
-//	Transparent palette entry: 31.
-//	+ palette 32 entries, not compressed
+//	+ palette 256 entries, not compressed
 //	+ 96 tiles Metatiled by 4x4 not compressed
-//	Total size: 64 + 6144 = 6208
+//	Total size: 512 + 6144 = 6656
 //
-//	Time-stamp: 2014-09-24, 16:57:31
+//	Time-stamp: 2016-12-18, 17:41:55
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.6
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -86,9 +85,10 @@ extern const unsigned int BaldosasPal[41];
 #define SpritesTilesLen 6144
 extern const unsigned int SpritesTiles[1536];
 
-#define SpritesPalLen 64
-extern const unsigned int SpritesPal[16];
+#define SpritesPalLen 512
+extern const unsigned int SpritesPal[128];
 
 #endif // GRIT_SPRITES_H
 
 //}}BLOCK(Sprites)
+
